748b1c0c8c3ed7c0a0cd24b96c5bee8102287f83
mdpadberg <mdpadberg@protonmail.com>
2024-08-28 18:54:49 +0200

    fix borrow checker issue

--- first_parser/src/main.rs
+++ first_parser/src/main.rs
@@ -31,44 +31,55 @@ fn isval(t: &Term) -> bool {
   }
 }
 
-fn eval1(t: Term) -> Term {
+enum MyResult {
+  Changed(Term),
+  Unchanged(Term)
+}
+
+impl MyResult {
+  fn flat_map(self) -> Term {
+    match self {
+        MyResult::Changed(value) => value,
+        MyResult::Unchanged(value) => value,
+    }
+  }
+}
+
+fn eval1(t: Term) -> MyResult {
   match t {
     Term::TmIf(_, t1, t2, t3) => {
       match *t1 {
-        Term::TmTrue(_) => *t2,
-        Term::TmFalse(_) => *t3,
-        _ => Term::TmIf(Info { line: 0 }, Box::new(eval1(*t1)), t2, t3),
+        Term::TmTrue(_) => MyResult::Changed(*t2),
+        Term::TmFalse(_) => MyResult::Changed(*t3),
+        _ => MyResult::Changed(Term::TmIf(Info { line: 0 }, Box::new(eval1(*t1).flat_map()), t2, t3)),
       }
     }
-    Term::TmSucc(_, t1) => Term::TmSucc(Info { line: 0 }, Box::new(eval1(*t1))),
+    Term::TmSucc(_, t1) => MyResult::Changed(Term::TmSucc(Info { line: 0 }, Box::new(eval1(*t1).flat_map()))),
     Term::TmPred(_, t1) => {
       match *t1 {
-        Term::TmZero(_) => Term::TmZero(Info { line: 0 }),
-        Term::TmSucc(_, nv1) if isnumericval(&*nv1) => *nv1,
-        _ => Term::TmPred(Info { line: 0 }, Box::new(eval1(*t1))),
+        Term::TmZero(_) => MyResult::Changed(Term::TmZero(Info { line: 0 })),
+        Term::TmSucc(_, nv1) if isnumericval(&*nv1) => MyResult::Changed(*nv1),
+        _ => MyResult::Changed(Term::TmPred(Info { line: 0 }, Box::new(eval1(*t1).flat_map()))),
       }
     }
     Term::TmIsZero(_, t1) => {
       match *t1 {
-        Term::TmZero(_) => Term::TmTrue(Info { line: 0 }),
-        Term::TmSucc(_, nv1) if isnumericval(&*nv1) => Term::TmFalse(Info { line: 0 }),
-        _ => Term::TmIsZero(Info { line: 0 }, Box::new(eval1(*t1))),
+        Term::TmZero(_) => MyResult::Changed(Term::TmTrue(Info { line: 0 })),
+        Term::TmSucc(_, nv1) if isnumericval(&*nv1) => MyResult::Changed(Term::TmFalse(Info { line: 0 })),
+        _ => MyResult::Changed(Term::TmIsZero(Info { line: 0 }, Box::new(eval1(*t1).flat_map()))),
       }
     }
-    _ => t,
+    _ => MyResult::Unchanged(t),
   }
 }
 
-/*
 fn eval(t: Term) -> Term {
   let t1 = eval1(t);
-  if t1 == t {
-    t
-  } else {
-    eval(t1)
+  match t1 {
+    MyResult::Changed(term) => eval(term),
+    MyResult::Unchanged(term) => term,
   }
 }
-*/
 
 
 
